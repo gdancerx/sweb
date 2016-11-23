9e4ad4e3a6b01845052d21234bc80a0a0171d29f
Denis Volkov <gdancerx@gmail.com>
2016-11-23 20:16:58 +0000

    s

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 rm -r /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled
+git config user.email "gdancerx@gmail.com"
+git config user.name "Denis Volkov"
+
