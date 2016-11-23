81b037d7f742739a42b83222c8dbe480897176ef
Denis Volkov <gdancerx@gmail.com>
2016-11-23 20:35:08 +0000

    final 2

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ rm -r /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled
 rm -r /etc/gunicorn.d/*
 ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d
-ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d
+ln -s /home/box/web/etc/ask.conf /etc/gunicorn.d
 git config user.email "gdancerx@gmail.com"
 git config user.name "Denis Volkov"
 
