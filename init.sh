af34e34351d19a900324cc43f65d4b75e63f20fd
gdancerx <gdancerx@gmail.com>
2016-11-23 23:31:37 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 rm -r /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled
+rm -r /etc/gunicorn.d/*
+ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d
+ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d
 git config user.email "gdancerx@gmail.com"
 git config user.name "Denis Volkov"
 
