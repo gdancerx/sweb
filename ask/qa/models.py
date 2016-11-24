7404da141cde3a7d53047cf78e1dc51544d87dee
Denis Volkov <gdancerx@gmail.com>
2016-11-24 13:53:55 +0000

    final 3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):
+    def new():
+        pass
+    def popular():
+        pass
+
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=150)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name = 'likes_set')
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField()
-    question = models.OneToOneField(Question)
+    question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
 
-class QuestionManager(models.Manager):
-    def new():
-        pass
-    def popular():
-        pass
-   
