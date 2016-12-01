843137f42044443d83e17bed0edca45a8806cb16
gdancerx <gdancerx@gmail.com>
2016-12-02 01:24:51 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^popular/$', views.get_popular, name='get_popular'),
     url(r'^question/(?P<question_id>\d+)/$', views.get_question, name='get_question'),
     url(r'^ask/$', views.add_question, name='add_question'),
-    url(r'^signup/$', views.signup.as_view(), name='signup'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html', 'extra_context': {'next': '/'}}),
     url(r'^admin/', include(admin.site.urls)),
 ]
