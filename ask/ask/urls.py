7bc8a5fe0a828251ba757193fa9a7f43fe18ddde
gdancerx <gdancerx@gmail.com>
2016-12-01 14:07:05 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,18 +1,23 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from qa import views
+
 admin.autodiscover()
 
-urlpatterns = patterns('qa.views',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'test'),
+    #url(r'^$', 'test'),
     # url(r'^login/', 'test'),
     # url(r'^signup/', 'test'),
-    url(r'^question/\d+/', 'test'),
+    #url(r'^question/\d+/', 'test'),
     # url(r'^ask/', 'test'),
-    url(r'^popular/', 'test'),
-    # url(r'^new/', 'test'),                       
+    #url(r'^popular/', 'test'),
+    # url(r'^new/', 'test'), 
+    url(r'^$', views.get_page, name='get_page'),
+    url(r'^popular/$', views.get_popular, name='get_popular'),
+    url(r'^question/(?P<question_id>\d+)/$', views.get_question, name='get_question'),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
