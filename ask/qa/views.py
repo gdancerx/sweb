d335fc39df703630aa5dc01903a1589b6e643e27
gdancerx <gdancerx@gmail.com>
2016-12-01 14:08:48 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.http import HttpResponse
+from qa.models import Question, User
 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+def get_page(request):
+    page = request.GET.get('page')
+    #questions = Question.objects.all().order_by('-added_at')
+    questions = Question.objects.new()
+    paginator = Paginator(questions, 10)
+    paginator.baseurl = '/?page='
+    q_to_show = paginator.page(page)
+    return render(request, 'page.html', {
+        'questions': q_to_show.object_list,
+        'paginator': paginator,
+        'page': page,
+    })
+
+def get_popular(request):
+    page = request.GET.get('page')
+    questions = Question.objects.popular()
+    paginator = Paginator(questions, 10)
+    paginator.baseurl = '/?page='
+    q_to_show = paginator.page(page)
+    return render(request, 'page.html', {
+        'questions': q_to_show.object_list,
+        'paginator': paginator,
+        'page': page,
+    })
+
+def get_question(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, 'question.html', {
+        'question': question,
+    })
+
 
