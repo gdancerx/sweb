f736dea1698dfbf79a081dc5b361fb604d802887
gdancerx <gdancerx@gmail.com>
2016-12-01 22:10:49 +0300

    Create forms.py

---
+++ ask/qa/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from qa.models import Question, Answer
+
+class AskForm(forms.Form):
+    title = forms.CharField(label="Title", max_length=150)
+    text = forms.CharField(label="Text of the Question", widget=forms.Textarea)
+    def clean(self):
+        return self.cleaned_data
+    def save(self):
+        q = Question(**self.cleaned_data)
+        q.save()
+        return q
+
+class AnswerForm(forms.Form):
+    text = forms.CharField(label="Answer", widget=forms.Textarea)
+    question = forms.IntegerField(label="", widget=forms.HiddenInput)
+    def clean(self):
+        return self.cleaned_data
+    def save(self):
+        q_id = self.cleaned_data['question']
+        q = Question.objects.get(id=q_id)
+        self.cleaned_data['question']=q
+        a = Answer(**self.cleaned_data)
+        a.save()
+        return a
