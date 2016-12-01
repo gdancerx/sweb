becda6a86fb2e6b0f39061332595a996b1fe5cc7
gdancerx <gdancerx@gmail.com>
2016-12-02 01:29:12 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from qa.models import Question, User
